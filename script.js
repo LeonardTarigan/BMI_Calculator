acf7f65a9886b22308933f1b4808d763defe9387
LeonardTarigan <leontrg32@gmail.com>
2022-04-08 21:48:41 +0700

    fixed NaN bug

--- script.js
+++ script.js
@@ -5,27 +5,32 @@ function count() {
     let message = document.querySelector(".message-out");
     let index = document.querySelector(".index-out");
 
-    if (height > 10) {
-        message.innerHTML = "Tinggi harus dalam meter!";
-        status.innerHTML = "-";
-        index.innerHTML = "-";
+    status.innerHTML = "-";
+    index.innerHTML = "-";
+
+    if (isNaN(height) || isNaN(weight)) {
+        message.innerHTML = "Input harus berupa angka!"
     } else {
-        let result = (weight / (height * height)).toFixed(1);
+        if (height > 10) {
+            message.innerHTML = "Tinggi harus dalam meter!";
+        } else {
+            let result = (weight / (height * height)).toFixed(1);
 
-        index.innerHTML = result;
+            index.innerHTML = result;
 
-        if (result < 18.5) {
-            status.innerHTML = "Underweight";
-            message.innerHTML = "Wah, perlu naikin berat badan nih";
-        } else if (result > 18.5 && result < 22.9) {
-            status.innerHTML = "Normal";
-            message.innerHTML = "Keren! Pertahankan ya.";
-        } else if (result > 22.9 && result < 29.9) {
-            status.innerHTML = "Overweight";
-            message.innerHTML = "Harus mulai diet nih. xixixi";
-        } else if (result > 29.9) {
-            status.innerHTML = "Obesitas";
-            message.innerHTML = "Kamu harus diet!";
+            if (result < 18.5) {
+                status.innerHTML = "Underweight";
+                message.innerHTML = "Wah, perlu naikin berat badan nih";
+            } else if (result > 18.5 && result < 22.9) {
+                status.innerHTML = "Normal";
+                message.innerHTML = "Keren! Pertahankan ya.";
+            } else if (result > 22.9 && result < 29.9) {
+                status.innerHTML = "Overweight";
+                message.innerHTML = "Harus mulai diet nih. xixixi";
+            } else if (result > 29.9) {
+                status.innerHTML = "Obesitas";
+                message.innerHTML = "Kamu harus diet!";
+            }
         }
     }
 }
